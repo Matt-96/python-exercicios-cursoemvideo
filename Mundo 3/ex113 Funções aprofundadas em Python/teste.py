56a90a9bc91465f66c28055fa18e63f2de8404d7
Matt-96 <Matheusdiasr9@gmail.com>
2026-03-20 12:22:54 -0300

    Ex 113 finalizado: Pequenas modificações no arquivo de teste

--- Mundo 3/ex113 Funções aprofundadas em Python/teste.py
+++ Mundo 3/ex113 Funções aprofundadas em Python/teste.py
@@ -1,11 +1,11 @@
 from funções_matematicas import leiaInt, leiaFloat
-n = leiaInt('Digite um número: ')
+n = leiaInt('Digite um número inteiro: ')
 if n is None:
     print('O usuário não informou nenhum valor.')
 else:
     print(f'Você digitou o número {n}')
 
-n = leiaFloat('Digite um número: ')
+n = leiaFloat('Digite um número real: ')
 
 if n is None:
     print('O usuário não informou nenhum valor.')
